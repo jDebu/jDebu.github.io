6d0714aa5c6f66b5d36d321cfaad10af98c565de
Jose Delgado <j2delgadob@gmail.com>
2015-04-07 12:36:51 -0500

    Mensaje contact

---
+++ public/index.php
@@ -0,0 +1,227 @@
+<?php error_reporting(0);?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>My Curriculum Vitae</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="css/stylecv.css"/>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat:400,700"/>
+    <link rel="shorcut icon" href="/favicon.ico"/>
+    <link rel="alternate" title="My RSS" type="application/rss+xml" href="/fedd.rss"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.css"/>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/maincv.css"/>
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js">			</script>
+  </head>
+  <body>
+    <header id="start">
+      <nav role="navigation" class="fazul navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="row row-eq-height">
+            <div class="col-xs-offset-1 col-xs-4 col-sm-offset-1 col-sm-4 col-md-offset-1 col-md-4">
+              <h1 class="hide">Jose Delgado</h1>
+              <h3 class="tceleste text-center">JOSE DELGADO</h3>
+            </div>
+            <div class="col-xs-offset-1 col-xs-5 col-sm-offset-1 col-sm-5 col-md-offset-1 col-md-5">
+              <ul class="list-inline voffset4">
+                <li><a href="#about" class="tblanco">ABOUT ME</a></li>
+                <li><a href="#experience" class="tblanco">PROFESIONAL EXPERIENCE</a></li>
+                <li><a href="#contact" class="tblanco">CONTACT</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <section id="about">
+      <div class="row fceleste">
+        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+          <article class="tblanco"><a href="#" class="tblanco">
+              <h2>+ABOUT</h2></a>
+            <p>Hola, mi nombre es Jose Delgado, soy desarrollador web y me gusta crear paginas web hermosas. Mi objetivo es hacer diseños web y aplicativos moviles simples, atractivos y faciles de usar para crear grandes y divertidas experiencias</p>
+          </article>
+          <section>
+            <div class="row">
+              <div class="col-xs-12 col-sm-6 col-md-6"><img src="img/jose.png" alt="foto" class="voffset6 foto img-circle img-responsive"/>
+                <div class="social"><a href="http://twitter.com/@j2Debu" class="icon-twitter"></a><a href="http://github.com/j2DelgadoB" class="icon-github"></a><a href="http://gplus.to/j2sedelgado" class="icon-google-plus"></a></div>
+              </div>
+              <div class="col-xs-12 col-sm-5 col-md-5">
+                <aside class="tblanco">
+                  <h3 class="hide">titulo de contenido de barra lateral</h3>
+                  <dl class="dl-horizontal voffset5">
+                    <dt>NAME</dt>
+                    <dd>JOSE DELGADO BUSTAMANTE</dd>
+                    <hr class="visible-phone"/>
+                    <dt>BORN</dt>
+                    <dd>22 SEPTEMBER 1991</dd>
+                    <hr class="visible-phone"/>
+                    <dt>ADDRESS</dt>
+                    <dd>LOS ALISOS.LIMA.PERU</dd>
+                    <hr class="visible-phone"/>
+                    <dt>EMAIL</dt>
+                    <dd><strong class="tnegro">J2DELGADOB@GMAIL.COM</strong></dd>
+                    <hr class="visible-phone"/>
+                    <dt>WEBSITE</dt>
+                    <dd><a href="#" class="tblanco">WWW.JDEBU.GITHUB.IO</a></dd>
+                    <hr class="visible-phone"/>
+                    <dt>PHONE</dt>
+                    <dd>+51 980 815 608</dd>
+                    <hr class="visible-phone"/>
+                  </dl>
+                </aside>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </section>
+    <section id="experience">
+      <div class="row fplomo">
+        <div class="col-xs-offset-1 col-xs-11 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+          <article>
+            <h2>+WORK EXPERIENCE</h2>
+            <p>He trabajado en empresas desarrolladoras de software, como Freelance y en proyectos propios.</p>
+            <p>Me gusta aprender algo nuevo cada día sobre tecnologías web y programación de la misma documentacion o experimentando con el codigo, apasionado investigador que también se mantiene al tanto del trabajo de otros developers.					</p>
+          </article>
+          <section>
+            <div id="expert" class="row">
+              <div class="col-xs-12 col-sm-7 col-md-7">
+                <h3>+EMPLOYMENT</h3>
+                <h4 class="tverde">WEB DEVELOPER</h4>
+                <dl>
+                  <dt>Peruserver<span class="pull-right label label-default">2013-2014</span></dt>
+                  <dd>Desarrolle proyectos con Django, fue una gran experiencia en donde encontre el lenguaje ideal en que quiero programar para web, con codigo muy ordenado y sintaxis perfecta Django es una de las mejores opciones.</dd>
+                  <dt>Research and Developer</dt>
+                  <dt>Ofermedia-Pacis<span class="pull-right label label-default">2012-2013</span></dt>
+                  <dd>Aunque ya tenia experiencia como Freelance, en Ofermedia fue mi primer trabajo donde Desarrolle una plataforma de ventas online, y en Pacis hice investigación de Sistemas MultiAgente. 	</dd>
+                </dl>
+                <h3>+EDUCATION</h3>
+                <h4 class="tverde">SYSTEMS ENGINEERING</h4>
+                <dl>
+                  <dt>National University of San Marcos<span class="pull-right label label-default">University Lima</span></dt>
+                  <dd> Estudiando el ultimo año la carrera de Ingenieria de Sistemas e Informatica.</dd>
+                </dl>
+                <h4 class="tverde">UX DESIGN, GRAPHIC DESIGN, WEB AND MOBILE DEVELOPER</h4>
+                <dl>
+                  <dt>Autodidact<span class="pull-right label label-default">web</span></dt>
+                  <dd>Yo defino características, los flujos de usuarios, contenido y condiciones de error. Siempre estoy en una lucha constante con las interfaces feas, aburridas y confusas.</dd>
+                </dl>
+              </div>
+              <div class="clearfix">				</div>
+              <div class="col-xs-12 col-sm-5 col-md-5">
+                <aside class="voffset6">
+                  <h3 class="hide">titulo de contenido de barra lateral</h3>
+                  <h4>GRAPHIC SKILLS</h4>
+                  <div class="row">
+                    <div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%" class="progress-bar fazul"><span>Photoshop</span></div>
+                      </div>
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%" class="progress-bar fazul"><span>Illustrator</span></div>
+                      </div>
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%" class="progress-bar fazul"><span>Indesign</span></div>
+                      </div>
+                    </div>
+                  </div>
+                  <h4>WEB DEVELOPMENT</h4>
+                  <div class="row">
+                    <div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%" class="progress-bar fceleste"><span>JQuery</span></div>
+                      </div>
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%" class="progress-bar fceleste"><span>PHP</span></div>
+                      </div>
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%" class="progress-bar fceleste"><span>Android									</span></div>
+                      </div>
+                    </div>
+                  </div>
+                  <h4>SOCIAL MEDIA</h4>
+                  <div class="row">
+                    <div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%" class="progress-bar fverde"><span>Facebook</span></div>
+                      </div>
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%" class="progress-bar fverde"><span>Twitter</span></div>
+                      </div>
+                      <div class="progress">
+                        <div role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%" class="progress-bar fverde"><span>Youtube									</span></div>
+                      </div>
+                    </div>
+                  </div>
+                </aside>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </section>
+    <section id="contact">
+      <div class="row fverde">
+        <div class="col-xs-offset-1 col-xs-11 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+          <section class="row-eq-height">
+            <div class="col-xs-7 col-sm-7 col-md-7">
+              <article class="tblanco">
+                <h2 class="visible-lg">+CONTACT						</h2>
+                <h2 class="visible-md">+CONTACT</h2>
+                <h3 class="visible-sm">+CONTACT</h3>
+                <h4 class="visible-xs">+CONTACT</h4>
+                <p>Déjame un mensaje utilizando el formulario de contacto.</p>
+                <form action="to_email.php" role="form" method="POST" name="form-contact">
+                  <div class="form-group">
+                    <label for="InputName">Nombre</label>
+                    <input type="text" name="nombre" id="InputName" placeholder="" class="form-control"/>
+                  </div>
+                  <div class="form-group">
+                    <label for="InputEmail">Email</label>
+                    <input type="email" name="email" id="InputEmail" placeholder="" class="form-control"/>
+                  </div>
+                  <div class="form-group">
+                    <label for="InputMessage">Mensaje</label>
+                    <textarea id="InputMessage" name="mensaje" rows="3" class="form-control"></textarea>
+                  </div>
+                  <button type="submit" name="submit" class="btn btn-primary">Enviar</button>
+                  <p><?php echo $respuesta;?></p>
+                </form>
+              </article>
+            </div>
+            <div class="col-xs-5 col-sm-5 col-md-5">
+              <article class="tblanco">
+                <h2 class="visible-lg">+SOCIAL</h2>
+                <h2 class="visible-md">+SOCIAL</h2>
+                <h3 class="visible-sm">+SOCIAL</h3>
+                <h4 class="visible-xs">+SOCIAL			</h4>
+                <p>O usted puede contactar conmigo en</p>
+                <ul class="list-inline">
+                  <li><a id="facebook" href="http://facebook.com/fdfd.dffd.9" target="_blank" class="icon-facebook2"></a></li>
+                  <li> <a id="twitter" href="http://twitter.com/@j2Debu" target="_blank" class="icon-twitter"></a></li>
+                  <li><a id="google" href="http://gplus.to/j2sedelgado" target="_blank" class="icon-google-plus"></a></li>
+                </ul>
+              </article>
+            </div>
+          </section>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="row fazul row">
+        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+          <div class="row row-eq-height">
+            <div class="col-xs-8 col-sm-8 col-md-8">
+              <p class="tblanco">Copyright 2014</p>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-md-2"><a href="#start" class="tblanco">Volver arriba <span class="glyphicon glyphicon-arrow-up"> </span></a></div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js">			</script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script>function mediaqueryresponse(mql){ if (mql.matches){ /*if media query matches*/console.log("The condition " + mql.media + " has been met");$("#expert").addClass("row-eq-height");}else{$("#expert").removeClass("row-eq-height");/*document.getElementById("expert").addClass("row-eq-height");*/console.log("Condition not met yet");}}var mql = window.matchMedia("only screen and (min-width : 992px)");mediaqueryresponse(mql); /*call listener function explicitly at run time*/mql.addListener(mediaqueryresponse);/*attach listener function to listen in on state changes*/</script>
+  </body>
+</html>
---
+++ public/to_email.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(0);
+if(!isset($_POST['submit'])) {
+  //no envia aun
+  $respuesta="";
+  header('Location: index.php');
+}
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+if (empty($nombre) || empty($email) || empty($mensaje)) {
+  $respuesta="El nombre, email y mensaje se deben ingresar";
+  header('Location: index.php');
+}
+$email_from ="j2delgadob@gmail.com";
+$email_subject = "Formulario web";
+$email_body="Ha recibido un mensaje de $nombre.\n".
+            "email:$email\n".
+            "Su mensaje fue:$mensaje";
+
+$to ="j2delgadob@gmail.com";
+$headers ="From: $email_from \n";
+
+mail($to,$email_subject,$email_body,$headers);
+$respuesta="mensaje enviado";
+echo $respuesta;
+
+header('Location: index.php#contact');
+
+?>
